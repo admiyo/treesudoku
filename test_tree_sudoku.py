7937f5c6d81ac3a737f68c561dddc1b5d2b7e93b
Adam Young <adam@younglogic.com>
2024-11-11 18:31:44 -0500

    test code  without running a new process

--- test_tree_sudoku.py
+++ test_tree_sudoku.py
@@ -1,21 +1,15 @@
 from treesudoku import tree_sudoku
 
-import subprocess
-check_data ="""
-Board:0483921657967345821251876493548132976729564138136798245372689514814253769695417382
-Board:1245981376169273584837564219976125438513498627482736951391657842728349165654812793
-Board:2462831957795426183381795426173984265659312748248567319926178534834259671517643892"""
-def test_end_to_end():
-    print ("Running Test")
-    p = subprocess.run(["python3", "./tree_sudoku.py"], capture_output=True)
-    output = p.stdout.decode("utf-8").split("\n")
-    output = "".join(output[:-2])
-    output = output.replace("-","").replace("|","")
-    output = output.replace(" ","").replace("\n","")
-    output = output.replace("Board","\nBoard")
-    print("comparing output ")
-    assert(len(check_data) == len(output))
-    assert(check_data == output)
-    print("OK")
+puzzles = {
+    "0": "483921657967345821251876493548132976729564138136798245372689514814253769695417382",
+    "1": "245981376169273584837564219976125438513498627482736951391657842728349165654812793",
+    "2": "462831957795426183381795426173984265659312748248567319926178534834259671517643892"
+}
+def test_sudoku_solver():
+    solver = tree_sudoku.SudokuSolver()
+    for key, solution in solver.solved_board_strings.items():
+        assert solver.solved_board_strings[key] == puzzles[key]
 
-test_end_to_end()
+
+
+test_sudoku_solver()
