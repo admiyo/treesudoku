e6cbea329584d9bd35198869c6cd53d83e324eb3
Adam Young <adam@younglogic.com>
2024-11-12 08:57:10 -0500

    broken test_advance

--- test_tree_sudoku.py
+++ test_tree_sudoku.py
@@ -30,8 +30,38 @@ puzzles = {
           "517643892")
 }
 
+puzzle0 = ("003020600" +
+           "900305001" +
+           "001806400" +
+           "008102900" +
+           "700000008" +
+           "006708200" +
+           "002609500" +
+           "800203009" +
+           "005010300")
+
 
 def test_sudoku_solver():
     solver = tree_sudoku.SudokuSolver(tree_sudoku.import_csv())
     for key, solution in solver.solved_board_strings.items():
         assert solver.solved_board_strings[key] == puzzles[key]
+
+
+def test_advance():
+    test_board = tree_sudoku.build_board(puzzle0)
+    node = tree_sudoku.Tree_Node(None, 0)
+    node.write(test_board)
+    assert (test_board[0][0] == '9')
+    node = node.advance(test_board)
+    node = node.advance(test_board)
+    node.write(test_board)
+    assert (test_board[0][3] == '0')
+    node = node.advance(test_board)
+    node.write(test_board)
+    assert (test_board[0][3] == '9')
+    back_node = node.retreat()
+    assert (test_board[0][3] == '0')
+    assert (node.value == "9")
+    back_node.write(test_board)
+    assert (test_board[0][2] == '3')
+    assert (back_node.board_spot == '02')
