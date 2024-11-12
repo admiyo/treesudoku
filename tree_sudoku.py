6ef1f576cf8877d2b334c0e2a097ec835f2abcbf
Adam Young <adam@younglogic.com>
2024-11-11 19:01:20 -0500

    interim commit clean up pep8

--- tree_sudoku.py
+++ tree_sudoku.py
@@ -39,17 +39,26 @@ class SudokuSolver:
         while index < MAX:
             current_board_filling_node = head_node
             test_board = copy.deepcopy(original_board)
-            test_board[int(current_board_filling_node.board_spot[0])][int(current_board_filling_node.board_spot[1])] = current_board_filling_node.value
+            curr_row = int(current_board_filling_node.board_spot[0]) 
+            curr_col = int(current_board_filling_node.board_spot[1])
+            test_board[curr_row][curr_col] = current_board_filling_node.value
             while current_board_filling_node.next_node:
                 current_board_filling_node = current_board_filling_node.next_node
-                test_board[int(current_board_filling_node.board_spot[0])][int(current_board_filling_node.board_spot[1])] = current_board_filling_node.value
-            if self.value_valid(test_board, int(current_node.board_spot[0]), int(current_node.board_spot[1])):
+                curr_row = int(current_board_filling_node.board_spot[0])
+                curr_col = int(current_board_filling_node.board_spot[1])
+                test_board[curr_row][curr_col] = current_board_filling_node.value
+            if self.value_valid(test_board,
+                                int(current_node.board_spot[0]),
+                                int(current_node.board_spot[1])):
                 index += 1
                 if index >= MAX:
                     continue
-                new_node = Tree_Node(index, self.board_index_table[index], current_node)
-                if test_board[int(new_node.board_spot[0])][int(new_node.board_spot[1])] != '0':
-                    new_node.value = test_board[int(new_node.board_spot[0])][int(new_node.board_spot[1])]
+                new_node = Tree_Node(index, self.board_index_table[index],
+                                     current_node)
+                row = int(new_node.board_spot[0])
+                col = int(new_node.board_spot[1])
+                if test_board[row][col] != '0':
+                    new_node.value = test_board[row][col]
                     new_node.possible_values = []
                 current_node.next_node = new_node
                 current_node = new_node
@@ -62,11 +71,11 @@ class SudokuSolver:
                     current_node.next()
                 else:
                     current_node.next()
- 
+
         return_string = ''
         current_node = head_node
         return_string += str(current_node.value)
-        while(current_node.next_node):
+        while (current_node.next_node):
             current_node = current_node.next_node
             return_string += str(current_node.value)
         return return_string
@@ -82,7 +91,7 @@ class SudokuSolver:
                 board_list.append(row_list)
             return_dict[str(index)] = board_list
         return return_dict
- 
+
     def print_board(self, board):
         for index1, row in enumerate(board):
             if index1 == 0 or index1 == 3 or index1 == 6:
@@ -90,11 +99,11 @@ class SudokuSolver:
             for index, char in enumerate(row):
                 print(char, '', end='')
                 if index == 2 or index == 5:
-                    print('| ', end = '')
+                    print('| ', end='')
             print('')
             if index1 == 8:
                 print('-' * 21)
- 
+
     def value_valid(self, board, row_index, column_index):
         row = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
         column = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
@@ -114,7 +123,8 @@ class SudokuSolver:
                 column.remove(number)
             else:
                 return False
-        box_indexes = self.box_index_table[self.find_box_of_index(str(row_index) + str(column_index))]
+        box_indexes = self.box_index_table[self.find_box_of_index(
+                                           str(row_index) + str(column_index))]
         for index in box_indexes:
             row = int(index[0])
             column = int(index[1])
@@ -125,9 +135,8 @@ class SudokuSolver:
                 square.remove(number)
             else:
                 return False
- 
         return True
- 
+
     def find_box_of_index(self, index):
         box = 'box not found'
         for each_box in self.box_index_table:
@@ -135,6 +144,7 @@ class SudokuSolver:
                 box = each_box
                 break
         return box
+
     def fill_box_index_table(self):
         boxes = {}
         box_center = [1, 1]
@@ -143,22 +153,27 @@ class SudokuSolver:
             for _each_box in range(BASIS):
                 box_list = []
                 for i in range(-1, 2):
-                    box_list.append(str(box_center[0] + i) + str(box_center[1] - 1))
-                    box_list.append(str(box_center[0] + i) + str(box_center[1]))
-                    box_list.append(str(box_center[0] + i) + str(box_center[1] + 1))
+                    box_list.append(str(box_center[0] + i) +
+                                    str(box_center[1] - 1))
+                    box_list.append(str(box_center[0] + i) +
+                                    str(box_center[1]))
+                    box_list.append(str(box_center[0] + i) +
+                                    str(box_center[1] + 1))
                 boxes[box_number] = box_list
                 box_number += 1
                 box_center[1] += BASIS
             box_center[0] += BASIS
             box_center[1] -= DIM
         return boxes
- 
+
     def fill_board_index_table(self):
         return_list = []
         for row in range(DIM):
             for column in range(DIM):
                 return_list.append(str(row) + str(column))
         return return_list
+
+
 class Tree_Node:
     def __init__(self, index, board_spot, last_node=None, next_node=None):
         self.possible_values = ['1', '2', '3', '4', '5', '6', '7', '8']
@@ -167,9 +182,10 @@ class Tree_Node:
         self.next_node = next_node
         self.last_node = last_node
         self.value = '9'
+
     def next(self):
         self.value = self.possible_values.pop()
- 
+
     def __str__(self):
         return self.value
 start = time.time()
